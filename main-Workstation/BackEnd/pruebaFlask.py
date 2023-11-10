76e69cd62d7439a560868b897caf677dcde159e5
juannsalto <115878705+juannsalto@users.noreply.github.com>
2023-11-10 18:44:08 -0300

    intento
    intetno conexion

--- main-Workstation/BackEnd/pruebaFlask.py
+++ main-Workstation/BackEnd/pruebaFlask.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hola, mundo!'
+def index():
+    return render_template('intento.html')
+
+@app.route('/pruebaConexion')
+def ejemplo():
+    mensaje = "ok"
+    return jsonify({'mensaje': mensaje})
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-hello ()
