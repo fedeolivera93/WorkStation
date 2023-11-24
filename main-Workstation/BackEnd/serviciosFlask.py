b95eed58c311d3b4cfa92e3c4cf9892117c92349
fedeolivera93 <141982362+fedeolivera93@users.noreply.github.com>
2023-11-24 20:22:54 -0300

    compras

--- main-Workstation/BackEnd/serviciosFlask.py
+++ main-Workstation/BackEnd/serviciosFlask.py
@@ -2,8 +2,12 @@ from flask import Flask, jsonify, render_template, request
 from logueo import ejecucion
 
 
-app = Flask(__name__, template_folder='C:\\Desarrollo\\Mis Proyectos\\WorkstationFedeOlivera\\main-Workstation\\FrontEnd\\HTML\\templateFiles', static_folder='C:\\Desarrollo\\Mis Proyectos\\WorkstationFedeOlivera\\main-Workstation\\FrontEnd\\HTML\\staticFiles' )
+app = Flask(__name__, template_folder='C:/Desarrollo/Workstation/main-Workstation/FrontEnd/HTML/templateFiles', static_folder='C:/Desarrollo/Workstation/main-Workstation/FrontEnd/HTML/staticFiles' )
 
+
+@app.route('/bienvenida')
+def bienvenida():
+    return render_template('bienvenida.html')
 
 @app.route('/inicio')
 def home():
@@ -12,6 +16,7 @@ def home():
 @app.route('/forgot_password')
 def forgot_password():
      return render_template('recuperarContraseña.html')
+
 
 @app.route('/login', methods=['POST'])
 def login(mail, password):
@@ -27,6 +32,11 @@ def login(mail, password):
         mensaje = 'error en verificacion usuario/contraseña'
 
     return jsonify({'mensaje': mensaje})
+
+@app.route('/compras')
+def compras():
+     return render_template('compras.html')
+
 
 
 if __name__ == '__main__':
