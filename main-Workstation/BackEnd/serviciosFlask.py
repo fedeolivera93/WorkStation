d667027e68b2c97fd315f1aab4c610eb6fbbc03b
juannsalto <115878705+juannsalto@users.noreply.github.com>
2023-11-24 20:01:54 -0300

    htmlFixCamposVacios

--- main-Workstation/BackEnd/serviciosFlask.py
+++ main-Workstation/BackEnd/serviciosFlask.py
@@ -18,16 +18,15 @@ def login(mail, password):
     mail = request.form['mail']
     password = request.form['password']
 
+    if mail or password == None:
+        return ('error en verificacion usuario/contraseña')
+
     if ejecucion(mail, password):
         mensaje = 'ok'
     else:
         mensaje = 'error en verificacion usuario/contraseña'
 
     return jsonify({'mensaje': mensaje})
-
-@app.route('/bienvenida')
-def forgot_password():
-     return render_template('bienvenida.html')
 
 
 if __name__ == '__main__':
